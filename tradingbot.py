1619e3aafc09745fbca5c7b5902d0ca63cf34f31
sailanka02 <saip.lanka02@gmail.com>
2024-09-14 16:54:56 -0500

    Added sentiment logic

--- tradingbot.py
+++ tradingbot.py
@@ -36,7 +36,14 @@ class MLTrader(Strategy):
         three_days_prior = today - Timedelta(days=3)
         return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
 
-    
+    def get_sentiment(self): 
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, 
+                                 start=three_days_prior, 
+                                 end=today) 
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment 
 
     def on_trading_iteration(self):
         cash, last_price, quantity = self.position_sizing() 
