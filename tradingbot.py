e408343801e52ab3aa030f21c79dde84a44e4f7f
sailanka02 <saip.lanka02@gmail.com>
2024-09-15 17:20:34 -0500

    finished sentiment analysis

--- tradingbot.py
+++ tradingbot.py
@@ -2,10 +2,9 @@ from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
-from datetime import datetime 
+from datetime import timedelta, datetime
 from alpaca_trade_api import REST 
-from timedelta import Timedelta 
-#from finbert_utils import estimate_sentiment
+from finbert_utils import estimate_sentiment
 
 API_KEY = "PKTZTO5YL2CHO1E29GBB"
 API_SECRET = "xOopCzPqUETGDj1hCWOORZb5Uf7gAt8X29UTJmVG"
@@ -33,7 +32,7 @@ class MLTrader(Strategy):
 
     def get_dates(self): 
         today = self.get_datetime()
-        three_days_prior = today - Timedelta(days=3)
+        three_days_prior = today - timedelta(days=3)
         return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
 
     def get_sentiment(self): 
